a9abeab50bec4593de14453095c00069007205b6
ilseon cho <test@naver.com>
2016-07-19 13:27:17 +0900

    조원 전체 합본

--- Bridge/src/main/java/com/brige/app/controller/userController.java
+++ Bridge/src/main/java/com/brige/app/controller/userController.java
@@ -1,5 +1,5 @@
 
-package com.woo.app.user;
+package com.brige.app.controller;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.woo.app.HomeController;
 
 @Controller
 public class userController {
