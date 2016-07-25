d197ab8222cf4c9fbac60d62305390f4d297a388
ilseon cho <test@naver.com>
2016-07-25 09:22:39 +0900

    0725

---
+++ Bridge/src/main/java/com/bridge/app/persistence/UserDAOImpl.java
@@ -0,0 +1,47 @@
+package com.bridge.app.persistence;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.bridge.app.domain.UserVO;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+	
+	
+	@Inject
+	private SqlSession sqlSession;
+
+	private static final String namespace = "com.brige.mappers.loginMapper";
+	
+
+	@Override
+	public UserVO readLogin(String userid, String userpw) throws Exception {
+		// 입력받은 userid와 pw를 비교하여 로그인을 해주는 메서드
+		
+		System.out.println("dao까지 왔음");
+		Map<String, Object> loginMap = new HashMap<String, Object>();
+		loginMap.put("userid", userid);
+		loginMap.put("userpw", userpw);
+	
+		return sqlSession.selectOne(namespace+".readLogin", loginMap);
+	}
+
+	@Override
+	public UserVO searchId(String username, String userbirthday) throws Exception {
+		// 이름과 생년월일으로 아이디 찾기
+		
+		System.out.println("dao까지 왔음");
+		Map<String, Object> idsearchMap = new HashMap<String, Object>();
+		idsearchMap.put("username", username);
+		idsearchMap.put("userbirthday", userbirthday);
+		
+		return sqlSession.selectOne(namespace+".searchId", idsearchMap);
+	}
+
+}
